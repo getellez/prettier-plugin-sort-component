a667e8d734522a04277c9727bb35f05e0afe7fa2
German Tellez <gertellezv@gmail.com>
2024-08-28 21:03:09 -0500

    Update example file

--- example_1.jsx
+++ example_1.jsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
+import { useState } from 'react';
 export const MyComponent = () => {
   useEffect(() => {
-    console.log('counter ::>', counter)
-  }, [counter])
+    console.log('counter ::>', counter);
+  }, [counter]);
   const currentDate = new Date().toISOString();
   const [counter, setCounter] = useState(0);
+  const [message, setMessage] = useState('');
   const increment = () => setCounter(counter + 1);
-  const reset = function(){
+  const reset = function () {
     setCounter(0);
-  }
-  
+  };
+
   return (
-  <>
-    <div>{counter}</div>;
-    <button onClick={increment}>Increment</button>
-    <button onClick={reset}>Reset</button>
-    <hr />
-    <h1>Current Date: {currentDate} </h1>
-  </>
-  )
+    <>
+      <div>{counter}</div>;<button onClick={increment}>Increment</button>
+      <button onClick={reset}>Reset</button>
+      <hr />
+      <h1>Current Date: {currentDate} </h1>
+    </>
+  );
 };
